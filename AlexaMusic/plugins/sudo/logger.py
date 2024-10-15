88637e756ad22094573604a22e165053cb26a034
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2024-10-15 10:19:07 +0530

    Update logger.py

--- AlexaMusic/plugins/sudo/logger.py
+++ AlexaMusic/plugins/sudo/logger.py
@@ -39,8 +39,8 @@ async def logger(client, message, _):
         await message.reply_text(_["log_3"])
     else:
         await message.reply_text(usage)
-        
-        @app.on_message(filters.command(["cookies"]) & SUDOERS)
+
+@app.on_message(filters.command(["cookies"]) & SUDOERS)
 @language
 async def logger(client, message, _):
     await message.reply_document("cookies/logs.csv")
